691aea1cd0de805f6f3d02d7ba1941201115e16e
Algy Tynan <algy.tynan@planetinnovation.com.au>
2026-03-13 13:39:08 +1100

    DEG-XXX: Fix touch coordinate mapping for ILI9881C display.
    Remove axis inversion in DEMO_ReadTouch so touch coordinates match
    the display orientation. The previous inversion caused touch input
    to be mapped upside down (180 degrees off).

--- src/board/lvgl_support.c
+++ src/board/lvgl_support.c
@@ -659,10 +659,10 @@ static void DEMO_ReadTouch(lv_indev_t * drv, lv_indev_data_t * data) {
     if (DEMO_PANEL_WIDTH != s_touchResolutionX) {
         touch_x = touch_x * DEMO_PANEL_WIDTH / s_touchResolutionX;
     }
-    data->point.x = s_touchResolutionX - 1 - touch_x;
-    
+    data->point.x = touch_x;
+
     if (DEMO_PANEL_HEIGHT != s_touchResolutionY) {
         touch_y = touch_y * DEMO_PANEL_HEIGHT / s_touchResolutionY;
     }
-    data->point.y = s_touchResolutionY - 1 - touch_y;
+    data->point.y = touch_y;
 }
